f8ec382d063bf995f9e5b4cccc821d73a3a524ab
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-10 16:44:51 +0100

    Delete Paziente directory

--- Paziente/F_taste_paziente/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from F_taste_paziente.models.paziente import PazienteModel
-from F_taste_paziente.models.consensi_utente import ConsensiUtenteModel
-from F_taste_paziente.models.log_consensi import LOGConsensi
-from F_taste_paziente.models.nutrizionista import NutrizionistaModel
-from F_taste_paziente.models.richiesta_aggiunta_paziente import RichiestaAggiuntaPazienteModel
-from F_taste_paziente.models.richiesta_revocata import RichiestaRevocataModel
-from F_taste_paziente.models.informativa import InformativaBreveModel
