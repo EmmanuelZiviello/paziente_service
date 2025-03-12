16247705e34b3094ea4d5997cbb27d8329e4e94e
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-12 03:05:08 +0100

    collegamento con nut e richieste

---
+++ Paziente/F_taste_paziente/schemas/consensi_utente.py
@@ -0,0 +1,17 @@
+# from flaskr import db
+from F_taste_paziente.ma import ma
+from F_taste_paziente.models.consensi_utente import ConsensiUtenteModel
+from marshmallow import fields
+class ConsensiUtenteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ConsensiUtenteModel
+        load_instance = True
+        # sqla_session = db.session
+        include_fk = True
+
+    condivisione_misurazioni_paziente = fields.Boolean(required=True)
+    storage_from_Google_fit = fields.Boolean(required=True)
+    storage_from_Health_kit = fields.Boolean(required=True)
+    management_user_consent = fields.Boolean(required=True)
+    statistic_user_consent = fields.Boolean(required=True)
+    trainingAI_user_consent= fields.Boolean(required=True)
